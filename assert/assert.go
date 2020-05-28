2ccaa3a8066aa731d557df49bcbd5ea18f2a4711
GerryLon <gerrylon@163.com>
2020-05-28 15:08:26 +0800

    add Equal

--- assert/assert.go
+++ assert/assert.go
@@ -2,10 +2,17 @@ package assert
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func Nil(a interface{}) {
 	if a != nil {
 		panic(fmt.Sprintf("%v should be nil", a))
+	}
+}
+
+func Equal(a, b interface{}) {
+	if !reflect.DeepEqual(a, b) {
+		panic(fmt.Sprintf("%v and %v are not equal", a, b))
 	}
 }
